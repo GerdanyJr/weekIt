46307fb22200cb3ba7b89d673a13fb0a03d29e74
GerdanyJr <asa_lindo@hotmail.com>
2024-10-16 18:24:13 -0300

    feat: create Student entity

---
+++ src/main/java/com/github/gerdanyJr/weekit/model/entities/Student.java
@@ -0,0 +1,35 @@
+package com.github.gerdanyJr.weekit.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "students")
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 50, nullable = false)
+    private String lastName;
+
+    @Column(length = 11, nullable = false, unique = true)
+    private String cpf;
+
+    @Column(length = 12, nullable = false, unique = true)
+    private String registrationNumber;
+}
