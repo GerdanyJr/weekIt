fc2cc8040720b032f935409cce8f508e12efd393
GerdanyJr <asa_lindo@hotmail.com>
2024-10-16 18:24:08 -0300

    feat: create Course entity

--- src/main/java/com/github/gerdanyJr/weekit/model/entities/Course.java
+++ src/main/java/com/github/gerdanyJr/weekit/model/entities/Course.java
@@ -20,7 +20,7 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 50, nullable = false)
+    @Column(length = 50, nullable = false, unique = true)
     private String name;
 
     @Column(length = 255, nullable = false)
