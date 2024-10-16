d61aca939d11c6534e8c02794e156076c51ff2b2
GerdanyJr <asa_lindo@hotmail.com>
2024-10-16 19:29:31 -0300

    feat: implement delete student endpoint

--- src/main/java/com/github/gerdanyJr/weekit/controller/StudentController.java
+++ src/main/java/com/github/gerdanyJr/weekit/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,5 +51,11 @@ public class StudentController {
     @PatchMapping("/{id}")
     public ResponseEntity<Student> update(@PathVariable Long id, @RequestBody @Valid CreateStudentReq req) {
         return ResponseEntity.ok(studentService.update(id, req));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        studentService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
