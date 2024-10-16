77c95b47993ed801fefeb89026fb5f5c08865384
GerdanyJr <asa_lindo@hotmail.com>
2024-10-16 18:23:53 -0300

    feat: create Course entity

---
+++ src/main/java/com/github/gerdanyJr/weekit/model/entities/Course.java
@@ -0,0 +1,28 @@
+package com.github.gerdanyJr.weekit.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "courses")
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 255, nullable = false)
+    private String description;
+}
