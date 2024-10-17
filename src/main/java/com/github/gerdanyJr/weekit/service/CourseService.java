4210b40d0c4711b87e629fde75b701ac4f88d49d
GerdanyJr <asa_lindo@hotmail.com>
2024-10-17 15:29:15 -0300

    feat: implement findAll on course service

--- src/main/java/com/github/gerdanyJr/weekit/service/CourseService.java
+++ src/main/java/com/github/gerdanyJr/weekit/service/CourseService.java
@@ -1,8 +1,11 @@
 package com.github.gerdanyJr.weekit.service;
 
+import java.util.List;
 import com.github.gerdanyJr.weekit.model.entities.Course;
 import com.github.gerdanyJr.weekit.model.req.CreateCourseReq;
 
 public interface CourseService {
     public Course createCourse(CreateCourseReq req);
+
+    public List<Course> findAll();
 }
