9ddb1c6f5b6a5bd36a0b9e90f19d739a2019b08e
GerdanyJr <asa_lindo@hotmail.com>
2024-12-12 13:54:34 -0300

    feat: create findAllParticipations method

--- src/main/java/com/github/gerdanyJr/weekit/service/ParticipationService.java
+++ src/main/java/com/github/gerdanyJr/weekit/service/ParticipationService.java
@@ -1,8 +1,12 @@
 package com.github.gerdanyJr.weekit.service;
 
+import java.util.List;
+
 import com.github.gerdanyJr.weekit.model.entities.Participation;
 import com.github.gerdanyJr.weekit.model.req.CreateParticipationReq;
 
 public interface ParticipationService {
     public Participation create(CreateParticipationReq req);
+
+    public List<Participation> findAll();
 }
