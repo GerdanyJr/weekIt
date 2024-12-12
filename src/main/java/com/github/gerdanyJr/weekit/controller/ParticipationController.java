91b064a4db79b335ade15db61bdc59a162d0d920
GerdanyJr <asa_lindo@hotmail.com>
2024-12-12 14:12:47 -0300

    feat: create findById endpoint

--- src/main/java/com/github/gerdanyJr/weekit/controller/ParticipationController.java
+++ src/main/java/com/github/gerdanyJr/weekit/controller/ParticipationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,10 @@ public class ParticipationController {
     @GetMapping
     public ResponseEntity<List<Participation>> findAll() {
         return ResponseEntity.ok(participationService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Participation> findByid(@PathVariable Long id) {
+        return ResponseEntity.ok(participationService.findById(id));
     }
 }
