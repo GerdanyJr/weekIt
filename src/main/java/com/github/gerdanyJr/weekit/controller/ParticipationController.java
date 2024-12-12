89ef9bcb2b9b47f645ea6bd663a13fcec053f382
GerdanyJr <asa_lindo@hotmail.com>
2024-12-12 13:54:53 -0300

    feat: create endpoint to find all participations

--- src/main/java/com/github/gerdanyJr/weekit/controller/ParticipationController.java
+++ src/main/java/com/github/gerdanyJr/weekit/controller/ParticipationController.java
@@ -1,6 +1,9 @@
 package com.github.gerdanyJr.weekit.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,10 @@ public class ParticipationController {
     @PostMapping
     public ResponseEntity<Participation> create(@RequestBody CreateParticipationReq req) {
         return ResponseEntity.ok(participationService.create(req));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Participation>> findAll() {
+        return ResponseEntity.ok(participationService.findAll());
     }
 }
