43afdac96c338a42a0fb0ef484e1f8a98a187000
GerdanyJr <asa_lindo@hotmail.com>
2024-12-12 12:23:34 -0300

    feat: add create Participation endpoint to ParticipationController

---
+++ src/main/java/com/github/gerdanyJr/weekit/controller/ParticipationController.java
@@ -0,0 +1,26 @@
+package com.github.gerdanyJr.weekit.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.gerdanyJr.weekit.model.entities.Participation;
+import com.github.gerdanyJr.weekit.model.req.CreateParticipationReq;
+import com.github.gerdanyJr.weekit.service.ParticipationService;
+
+@RestController
+@RequestMapping("/participacoes")
+public class ParticipationController {
+    private final ParticipationService participationService;
+
+    public ParticipationController(ParticipationService participationService) {
+        this.participationService = participationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Participation> create(@RequestBody CreateParticipationReq req) {
+        return ResponseEntity.ok(participationService.create(req));
+    }
+}
