16a10f97763ebc1606e86163d623178bbd524d36
GerdanyJr <asa_lindo@hotmail.com>
2024-12-12 09:39:21 -0300

    feat: add enpoint to create course

--- src/main/java/com/github/gerdanyJr/weekit/controller/CourseController.java
+++ src/main/java/com/github/gerdanyJr/weekit/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,6 +44,11 @@ public class CourseController {
     @GetMapping("/search")
     public ResponseEntity<List<Course>> searchByName(@RequestParam(name = "name", required = true) String name) {
         return ResponseEntity.ok(courseService.searchByName(name));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable("id") Long id, @RequestBody CreateCourseReq req) {
+        return ResponseEntity.ok(courseService.update(id, req));
     }
 
 }
